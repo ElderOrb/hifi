360ba4d2eed9c43b4e631618af34732cfd4118c0
Dante Ruiz <danteruiz102@gmail.com>
2018-04-24 16:25:22 -0700

    adding some functionality to avatar bookmarks

--- interface/src/AvatarBookmarks.h
+++ interface/src/AvatarBookmarks.h
@@ -35,6 +35,9 @@ public slots:
      * @function AvatarBookmarks.addBookMark
      */
     void addBookmark();
+    void addBookmark(QString bookmarkName) {}
+    void removeBookmark(QString bookmark) {}
+    QVariantMap getBookmarks() { return _bookmarks; }
 
 protected:
     void addBookmarkToMenu(Menu* menubar, const QString& name, const QVariant& bookmark) override;
